8ff1ac331f37de44ea9696f16182c6b676efac5b
GuyMiNgabirano <ngabiranog8@gmail.com>
2025-05-30 11:17:51 +0200

    modification de index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* Etape 1 :
+// Etape 1 :
 const fs = require("fs");
 
 // 1. Création d'un fichier document avec un message
@@ -6,35 +6,35 @@ fs.writeFileSync("document.txt", "Iwanyu murakunda kuryama mu gitondo");
 
 // 2. Lecture du fichier document et affichage de son contenu
 const content = fs.readFileSync("document.txt", "utf-8");
-console.log(content);*/
+console.log(content);
 
-/* Etape 2 :
+// Etape 2 :
 const { multiply } = require('./functions');
-console.log(multiply(10, 2));*/
+console.log(multiply(10, 2));
 
-/* Etape 3 :
+// Etape 3 :
 const { writeFileSync } = require('fs');
 
 const modeData = [
     {id: 1, name: 'Guy', age: 30}
 ]
-const modData = writeFileSync('data.json', JSON.stringify(modeData, null, 2));*/
+const modData = writeFileSync('data.json', JSON.stringify(modeData, null, 2));
 
-/* Etape 4 : récupération de données depuis une API avec postman-request
+// Etape 4 : récupération de données depuis une API avec postman-request
 const request = require('postman-request');
 
 request('https://jsonplaceholder.typicode.com/users', (error, response, body) => {
     const data = JSON.parse(body);
     console.log(data);
-});*/
+});
 
-/* Etape 5 : récupération de données depuis une API avec fetch
+// Etape 5 : récupération de données depuis une API avec fetch
 
 fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
-    .then(data => console.log(data));*/
+    .then(data => console.log(data));
 
-/*const parameters = {
+const parameters = {
     access_key: '67c9b72b823267b2a6a8276a7d89a551',
     query: 'New york',
     units: 'b',
@@ -42,4 +42,4 @@ fetch('https://jsonplaceholder.typicode.com/users')
 
 fetch('http://api.weatherstack.com/current?access_key='+parameters.access_key+'&query='+parameters.query+'&units='+parameters.units)
     .then(response => response.json())
-    .then(data => console.log(data));*/
+    .then(data => console.log(data));
